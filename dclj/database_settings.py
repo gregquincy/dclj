c2e60869e6a6da9048a91f518d70bb29fa3b5aba
archloy <Archloy@users.noreply.github.com>
2017-03-17 11:51:39 +0100

    initialize repository

---
+++ dclj/database_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'swift',
+            "USER" : "swift",
+            "PASSWORD" : "swift",
+            "HOST" : "127.0.0.1",
+            "PORT" : "5432",
+    }
+}
+
