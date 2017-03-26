6e9a146061d7a14a05df4c4eb4a624ae1af77398
archloy <Archloy@users.noreply.github.com>
2017-03-26 01:42:45 +0100

    Create server specific configuration file

--- dclj/settings.py
+++ dclj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 from . import database_settings as dbcfg
+from . import serverConfig as srv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +28,7 @@ SECRET_KEY = 'iv@a*!)4w@&on-lrg%bsb4e9$=3_fc=&mbph5^dfte(&noe_(!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = srv.HOSTS
 
 
 # Application definition
