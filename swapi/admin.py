ebe3998c7beb6d82e019f66c06aab9e6347e72bc
archloy <Archloy@users.noreply.github.com>
2017-03-26 01:30:42 +0100

    Add models to admin interface

--- swapi/admin.py
+++ swapi/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Activity)
+admin.site.register(models.ActivityField)
+admin.site.register(models.Field)
+admin.site.register(models.Media)
+admin.site.register(models.MediaType)
+admin.site.register(models.Report)
+admin.site.register(models.ReportActivityField)
+
+
